c6adbdaf1bbcc10035d0162d8a9278203ef91ddb
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-07-03 20:20:51 +0530

    Added:Flip Coin and print percentage of Heads and Tails

---
+++ src/main/java/com/bridgelabz/FlipCoin.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class FlipCoin {
+    public static void main(String[] args){
+        if(Math.random()<0.5){
+            System.out.println("Tails");
+        }
+            else{
+            System.out.println("Heads");
+        }
+
+    }
+}
